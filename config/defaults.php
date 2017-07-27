bf4dc0b6cdc99ea9a7ce5570c0be9beccced4a3b
Alain Schlesser <alain.schlesser@gmail.com>
2017-07-27 20:28:53 +0200

    Add comments to default config file.

--- config/defaults.php
+++ config/defaults.php
@@ -11,19 +11,31 @@
 
 namespace BrightNucleus\View;
 
+/*
+ * Engine finder default configuration.
+ */
 $engineFinder = [
+    // Class to use for instantiating the EngineFinder implementation.
     Engine\EngineFinder::CLASS_NAME_KEY => Engine\BaseEngineFinder::class,
+    // Engine implementations to register with the EngineFinder.
     Engine\EngineFinder::ENGINES_KEY    => [
         'PHPEngine' => Engine\PHPEngine::class,
     ],
+    // Null object implementation to use with the EngineFinder.
     Engine\EngineFinder::NULL_OBJECT    => Engine\NullEngine::class,
 ];
 
+/*
+ * View finder default configuration.
+ */
 $viewFinder = [
+    // Class to use for instantiating the ViewFinder implementation.
     View\ViewFinder::CLASS_NAME_KEY => View\BaseViewFinder::class,
+    // View implementations to register with the ViewFinder.
     View\ViewFinder::VIEWS_KEY      => [
         'BaseView' => View\BaseView::class,
     ],
+    // Null object implementation to use with the ViewFinder.
     View\ViewFinder::NULL_OBJECT    => View\NullView::class,
 ];
 
